22b1dfc0c4516004554a33c762b7f9d38da0c187
Ben Shanfelder <benshan@gmail.com>
2012-01-04 21:49:33 -0800

    Commit mods to window project

--- Pressure/Pressure/main.m
+++ Pressure/Pressure/main.m
@@ -1,5 +1,21 @@
 //
-// Copyright Ben Shanfelder. All rights reserved.
+// This file is part of Pressure.
+//
+// Pressure is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Pressure is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Pressure.  If not, see <http://www.gnu.org/licenses/>.
+//
+//  main.m
+//  Created by Ben Shanfelder on 1/4/12.
 //
 
 #import <Cocoa/Cocoa.h>
