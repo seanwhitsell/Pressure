a4d2dfa93d88f27ccabbd50197591d73ddb23368
Sean Whitsell <sean.whitsell@gmail.com>
2012-01-09 09:18:15 -0500

    Adding the Graph View Controller

---
+++ Pressure/Pressure/GraphViewController.h
@@ -0,0 +1,13 @@
+//
+//  GraphViewController.h
+//  Pressure
+//
+//  Created by Sean Whitsell on 1/8/12.
+//  Copyright (c) 2012 Cisco Systems, Inc. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface GraphViewController : NSViewController
+
+@end
---
+++ Pressure/Pressure/GraphViewController.m
@@ -0,0 +1,23 @@
+//
+//  GraphViewController.m
+//  Pressure
+//
+//  Created by Sean Whitsell on 1/8/12.
+//  Copyright (c) 2012 Cisco Systems, Inc. All rights reserved.
+//
+
+#import "GraphViewController.h"
+
+@implementation GraphViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
