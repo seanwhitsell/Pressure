1ee94de5636994eaf88afbd2651a93136b7c3f20
Sean Whitsell <sean.whitsell@gmail.com>
2012-01-15 15:28:08 -0500

    Updating the OpenSource attribution

--- Pressure/Pressure/ReadingViewController.h
+++ Pressure/Pressure/ReadingViewController.h
@@ -1,10 +1,23 @@
 //
+// This file is part of Pressure.
+//
+// Pressure is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Pressure is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Pressure.  If not, see <http://www.gnu.org/licenses/>.
+//
 //  ReadingViewController.h
 //  Pressure
 //
-//  Created by Sean Whitsell on 1/12/12.
-//  Copyright (c) 2012 Cisco Systems, Inc. All rights reserved.
-//
+//  Created by Sean Whitsell on 1/11/12.
 
 #import <Cocoa/Cocoa.h>
 #import "PXListView.h"
