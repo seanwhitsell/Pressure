c12ac1d61c26459ae67ff5f357a53d129aaf0721
Sean Whitsell <sean.whitsell@gmail.com>
2012-03-20 17:00:26 -0400

    Adding a filter by User

---
+++ Pressure/Pressure/UserFilter.h
@@ -0,0 +1,14 @@
+//
+//  UserFilter.h
+//  Pressure
+//
+//  Created by Sean Whitsell on 3/19/12.
+//  Copyright (c) 2012 Cisco Systems, Inc. All rights reserved.
+//
+
+#ifndef Pressure_UserFilter_h
+#define Pressure_UserFilter_h
+
+
+
+#endif
---
+++ Pressure/Pressure/UserFilter.m
@@ -0,0 +1,9 @@
+//
+//  UserFilter.c
+//  Pressure
+//
+//  Created by Sean Whitsell on 3/19/12.
+//  Copyright (c) 2012 Cisco Systems, Inc. All rights reserved.
+//
+
+#include <stdio.h>
